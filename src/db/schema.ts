a0d6c4c4f0b95c87999b7632578ab7ffa50d7be3
fawwazabrials <18221067@std.stei.itb.ac.id>
2024-04-23 22:29:51 +0700

    feat: update database schema

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,11 +1,26 @@
 import { createId } from '@paralleldrive/cuid2';
 import { InferSelectModel, relations } from 'drizzle-orm';
-import { index, json, pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import {
+  index,
+  integer,
+  json,
+  pgTable,
+  primaryKey,
+  text,
+  timestamp,
+} from 'drizzle-orm/pg-core';
 import webpush from 'web-push';
 
 export const users = pgTable('users', {
   id: text('id').primaryKey().$defaultFn(createId),
+  nim: text('nim').unique().notNull(),
   email: text('email').unique().notNull(),
+  full_name: text('full_name'),
+  jurusan: text('jurusan'),
+  asal_kampus: text('asal_kampus'),
+  angkatan: integer('angkatan'),
+  jenis_kelamin: text('jenis_kelamin'),
+  status_keanggotaan: text('status_keanggotaan'),
   createdAt: timestamp('created_at', { withTimezone: true })
     .notNull()
     .defaultNow(),
@@ -15,6 +30,9 @@ export type User = InferSelectModel<typeof users>;
 
 export const usersRelation = relations(users, ({ many }) => ({
   pushSubscriptions: many(pushSubscriptions),
+  infos: many(infos),
+  medias: many(medias),
+  userReadInfos: many(userReadInfos),
 }));
 
 export const pushSubscriptions = pgTable(
@@ -32,6 +50,8 @@ export const pushSubscriptions = pgTable(
   }),
 );
 
+export type PushSubscription = InferSelectModel<typeof pushSubscriptions>;
+
 export const pushSubscriptionsRelation = relations(
   pushSubscriptions,
   ({ one }) => ({
@@ -41,3 +61,118 @@ export const pushSubscriptionsRelation = relations(
     }),
   }),
 );
+
+export const googleSubscriptions = pgTable('google_subscriptions', {
+  userId: text('user_id')
+    .references(() => users.id)
+    .primaryKey(),
+  idToken: text('id_token').notNull(),
+  refreshToken: text('refresh_token').notNull(),
+  scope: text('scope').notNull(),
+  expiresIn: integer('expires_in').notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+});
+
+export type GoogleSubscription = InferSelectModel<typeof googleSubscriptions>;
+
+export const googleSubscriptionsRelation = relations(
+  googleSubscriptions,
+  ({ one }) => ({
+    user: one(users, {
+      fields: [googleSubscriptions.userId],
+      references: [users.id],
+    }),
+  }),
+);
+
+export const infos = pgTable('infos', {
+  id: text('id').primaryKey().$defaultFn(createId),
+  creatorId: text('creator_id').references(() => users.id),
+  content: text('content').notNull(),
+  forAngkatan: integer('for_angkatan'),
+  forMatakuliah: text('for_matakuliah'),
+  forClass: text('for_class'),
+  createdAt: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+});
+
+export type Info = InferSelectModel<typeof infos>;
+
+export const infosRelation = relations(infos, ({ one, many }) => ({
+  creator: one(users, {
+    fields: [infos.creatorId],
+    references: [users.id],
+  }),
+  medias: many(medias),
+  userReadInfos: many(userReadInfos),
+  infoMedias: many(infoMedias),
+}));
+
+export const medias = pgTable('medias', {
+  id: text('id').primaryKey().$defaultFn(createId),
+  creatorId: text('creator_id').references(() => users.id),
+  name: text('name').unique().notNull(),
+  type: text('type').notNull(),
+  url: text('url').notNull(),
+  createdAt: timestamp('created_at', { withTimezone: true })
+    .notNull()
+    .defaultNow(),
+});
+
+export type Media = InferSelectModel<typeof medias>;
+
+export const mediasRelation = relations(medias, ({ one, many }) => ({
+  creator: one(users, {
+    fields: [medias.creatorId],
+    references: [users.id],
+  }),
+  infos: many(infos),
+}));
+
+export const userReadInfos = pgTable(
+  'user_read_infos',
+  {
+    userId: text('user_id').references(() => users.id),
+    infoId: text('info_id').references(() => infos.id),
+    createdAt: timestamp('created_at', { withTimezone: true })
+      .notNull()
+      .defaultNow(),
+  },
+  (t) => ({
+    pk: primaryKey({ columns: [t.userId, t.infoId] }),
+  }),
+);
+
+export const userReadInfosRelation = relations(userReadInfos, ({ one }) => ({
+  user: one(users, {
+    fields: [userReadInfos.userId],
+    references: [users.id],
+  }),
+  info: one(infos, {
+    fields: [userReadInfos.infoId],
+    references: [infos.id],
+  }),
+}));
+
+export const infoMedias = pgTable(
+  'info_medias',
+  {
+    infoId: text('info_id').references(() => infos.id),
+    mediaId: text('media_id').references(() => medias.id),
+  },
+  (t) => ({ pk: primaryKey({ columns: [t.infoId, t.mediaId] }) }),
+);
+
+export const infoMediasRelation = relations(infoMedias, ({ one }) => ({
+  info: one(infos, {
+    fields: [infoMedias.infoId],
+    references: [infos.id],
+  }),
+  media: one(medias, {
+    fields: [infoMedias.mediaId],
+    references: [medias.id],
+  }),
+}));
