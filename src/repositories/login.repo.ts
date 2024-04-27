cdae64c25c723d78a7e518e87d801a57dac97ec4
fawwazabrials <18221067@std.stei.itb.ac.id>
2024-04-27 16:02:14 +0700

    style: switch to camelCase

--- src/repositories/login.repo.ts
+++ src/repositories/login.repo.ts
@@ -2,7 +2,7 @@ import { eq } from 'drizzle-orm';
 import { Database } from '~/db/drizzle';
 import { users } from '~/db/schema';
 
-export async function FindUserByEmail(db: Database, email: string) {
+export async function findUserByEmail(db: Database, email: string) {
   return await db.query.users.findFirst({
     where: eq(users.email, email),
   });
