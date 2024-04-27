518ce9ec38aa7f36d0fff4c2e2f017018317fd2f
fawwazabrials <18221067@std.stei.itb.ac.id>
2024-04-27 23:10:08 +0700

    docs: add example and change unauthorized response

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -43,6 +43,18 @@ export const authCallbackRoute = createRoute({
       content: {
         'application/json': {
           schema: JWTPayloadSchema,
+          example: {
+            id: 'hijtlb2yuy8dsssyy6cgo1fv',
+            nim: '18221000',
+            email: '18221000@std.stei.itb.ac.id',
+            full_name: 'Dr. Asep Spakbor',
+            jurusan: 'Sistem dan Teknologi Informasi',
+            asal_kampus: 'Ganesha',
+            angkatan: 2021,
+            jenis_kelamin: 'Laki-laki',
+            status_keanggotaan: 'Anggota Biasa',
+            picture: 'https://example.com/picture.jpg',
+          },
         },
       },
       description: 'Login succesful',
@@ -77,6 +89,18 @@ export const selfRoute = createRoute({
       content: {
         'application/json': {
           schema: JWTPayloadSchema,
+          example: {
+            id: 'hijtlb2yuy8dsssyy6cgo1fv',
+            nim: '18221000',
+            email: '18221000@std.stei.itb.ac.id',
+            full_name: 'Dr. Asep Spakbor',
+            jurusan: 'Sistem dan Teknologi Informasi',
+            asal_kampus: 'Ganesha',
+            angkatan: 2021,
+            jenis_kelamin: 'Laki-laki',
+            status_keanggotaan: 'Anggota Biasa',
+            picture: 'https://example.com/picture.jpg',
+          },
         },
       },
       description: 'Login succesful',
