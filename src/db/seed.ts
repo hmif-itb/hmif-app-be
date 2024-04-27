a6db9d5e66c8e64bce4a50449088e60146fc7003
frendysanusi05 <frendyvivo5@gmail.com>
2024-04-27 11:00:43 +0700

    feat: add user seeding validation, change filepath

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -4,6 +4,8 @@ import { users } from './schema';
 import { drizzle } from 'drizzle-orm/postgres-js';
 import fs from 'fs';
 import { parse } from 'csv-parse';
+import { z } from 'zod';
+import { createInsertSchema } from 'drizzle-zod';
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL is required');
@@ -13,22 +15,38 @@ const client = postgres(process.env.DATABASE_URL, { max: 1 });
 const db = drizzle(client);
 
 export async function runUserSeed() {
-  const filePath = 'src/db/database-anggota-hmif.csv';
+  const filePath = 'src/db/seed/database.csv';
   const data: Array<typeof users.$inferInsert> = [];
+
+  const dataSchema = createInsertSchema(users, {
+    email: z
+      .string()
+      .email()
+      .refine((value) => value.endsWith('@std.stei.itb.ac.id')),
+    jurusan: z.enum(['Teknik Informatika', 'Sistem dan Teknologi Informasi']),
+    asal_kampus: z.enum(['Ganesha', 'Jatinangor']),
+    jenis_kelamin: z.enum(['Laki-laki', 'Perempuan']),
+    status_keanggotaan: z.enum([
+      'Anggota Biasa',
+      'Anggota Kehormatan',
+      'Anggota Muda',
+    ]),
+  });
 
   fs.createReadStream(filePath)
     .pipe(parse({ delimiter: ',', from_line: 2 }))
     .on('data', (row) => {
-      data.push({
+      const user = dataSchema.parse({
         nim: row[1],
         email: row[6],
         full_name: row[2],
         jurusan: row[3],
         asal_kampus: row[7],
-        angkatan: row[0],
+        angkatan: +row[0],
         jenis_kelamin: row[4],
         status_keanggotaan: row[5],
       });
+      data.push(user);
     })
     .on('end', () => {
       db.insert(users)
