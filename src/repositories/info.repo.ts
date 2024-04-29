d6f0eaa3815118b954d0d28b80e156cfc1d02d20
arsaizdihar <arsadihar@gmail.com>
2024-04-29 13:09:31 +0700

    refactor: media repository interaction

--- src/repositories/info.repo.ts
+++ src/repositories/info.repo.ts
@@ -2,10 +2,7 @@ import { InferInsertModel } from 'drizzle-orm';
 import { Database } from '~/db/drizzle';
 import { firstSure } from '~/db/helper';
 import { infos, userReadInfos } from '~/db/schema';
-import {
-  createInfoMediaTransaction,
-  createMediaFromUrlTransaction,
-} from './media.repo';
+import { createInfoMedia, createMediasFromUrl } from './media.repo';
 
 /**
  * Create an info.
@@ -15,43 +12,30 @@ export async function createInfo(
   data: Omit<InferInsertModel<typeof infos>, 'createdAt'>,
   mediaUrls: string[] | undefined,
 ) {
+  // transaction drizzle auto rollback kalo error
   return await db.transaction(async (tx) => {
-    try {
-      const newInfo = await tx
-        .insert(infos)
-        .values(data)
-        .returning()
-        .then(firstSure);
+    const newInfo = await tx
+      .insert(infos)
+      .values(data)
+      .returning()
+      .then(firstSure);
 
-      if (mediaUrls) {
-        const newMedias = await Promise.all(
-          mediaUrls.map(async (mediaUrl) => {
-            return await createMediaFromUrlTransaction(
-              tx,
-              mediaUrl,
-              newInfo.creatorId,
-            );
-          }),
-        );
+    if (mediaUrls) {
+      const newMedias = await createMediasFromUrl(
+        tx,
+        mediaUrls,
+        newInfo.creatorId,
+      );
 
-        await Promise.all(
-          newMedias.map(async (media) => {
-            return await createInfoMediaTransaction(tx, {
-              infoId: newInfo.id,
-              mediaId: media.id,
-            });
-          }),
-        );
-      }
-      return newInfo;
-    } catch (err) {
-      try {
-        // Ini hack biar kalo rollback gagal, errornya tetep di throw
-        tx.rollback();
-      } catch (err2) {
-        throw err;
-      }
+      await createInfoMedia(
+        tx,
+        newMedias.map((media) => ({
+          infoId: newInfo.id,
+          mediaId: media.id,
+        })),
+      );
     }
+    return newInfo;
   });
 }
 
--- src/repositories/media.repo.ts
+++ src/repositories/media.repo.ts
@@ -1,45 +1,37 @@
-import { ExtractTablesWithRelations, InferInsertModel } from 'drizzle-orm';
-import { PgTransaction } from 'drizzle-orm/pg-core';
-import { PostgresJsQueryResultHKT } from 'drizzle-orm/postgres-js';
+import { InferInsertModel } from 'drizzle-orm';
+import { Database } from '~/db/drizzle';
 import { firstSure } from '~/db/helper';
 import { infoMedias, medias } from '~/db/schema';
-import * as schema from '~/db/schema';
 
 // EXAMPLE URL -> https://pub-45e54d5755814b02b87e024df83efb57.r2.dev/r176r3qcuqs3hg8o3dm93n35-asrielblunt.jpg
-export const createMediaFromUrlTransaction = async (
-  tx: PgTransaction<
-    PostgresJsQueryResultHKT,
-    typeof schema,
-    ExtractTablesWithRelations<typeof schema>
-  >,
-  url: string,
+export const createMediasFromUrl = async (
+  db: Database,
+  urls: string[],
   creatorId: string | null,
 ) => {
-  const file = url.split('/').at(-1); // asrielblunt.jpg
-  if (!file) {
-    throw Error('Invalid URL');
-  }
-  const [name, type] = file.split('.');
-
-  return await tx
+  return await db
     .insert(medias)
-    .values({
-      creatorId,
-      name,
-      type,
-      url,
-    })
-    .returning()
-    .then(firstSure);
+    .values(
+      urls.map((url) => {
+        const file = url.split('/').at(-1); // asrielblunt.jpg
+        if (!file) {
+          throw Error('Invalid URL');
+        }
+        const [name, type] = file.split('.');
+        return {
+          creatorId,
+          name,
+          type,
+          url,
+        };
+      }),
+    )
+    .returning();
 };
 
-export const createInfoMediaTransaction = async (
-  tx: PgTransaction<
-    PostgresJsQueryResultHKT,
-    typeof schema,
-    ExtractTablesWithRelations<typeof schema>
-  >,
-  data: Omit<InferInsertModel<typeof infoMedias>, 'createdAt'>,
+export const createInfoMedia = async (
+  db: Database,
+  data: Array<Omit<InferInsertModel<typeof infoMedias>, 'createdAt'>>,
 ) => {
-  return await tx.insert(infoMedias).values(data).returning().then(firstSure);
+  return await db.insert(infoMedias).values(data).returning().then(firstSure);
 };
