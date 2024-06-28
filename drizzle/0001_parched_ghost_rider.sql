5f5191b49547fb0afc9680bf59eeddc69c52bdfd
Fawwaz Abrial Saffa <18221067@std.stei.itb.ac.id>
2024-06-28 22:01:22 +0700

    feat: add new db schema for user roles

---
+++ drizzle/0001_parched_ghost_rider.sql
@@ -0,0 +1,10 @@
+CREATE TABLE IF NOT EXISTS "user_roles" (
+	"user_id" text NOT NULL,
+	"role" text NOT NULL
+);
+--> statement-breakpoint
+DO $$ BEGIN
+ ALTER TABLE "user_roles" ADD CONSTRAINT "user_roles_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
+EXCEPTION
+ WHEN duplicate_object THEN null;
+END $$;
