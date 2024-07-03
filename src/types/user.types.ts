f068852e52a686d5e2a1fc9dcfc55dc5c80545a7
ValentinoTriadi <valent122016@gmail.com>
2024-07-03 22:06:00 +0700

    feat: add nim finder schema and modify user schema createdAt

--- src/types/user.types.ts
+++ src/types/user.types.ts
@@ -1,4 +1,7 @@
 import { createSelectSchema } from 'drizzle-zod';
+import { z } from 'zod';
 import { users } from '~/db/schema';
 
-export const UserSchema = createSelectSchema(users);
+export const UserSchema = createSelectSchema(users, {
+  createdAt: z.union([z.string(), z.date()]),
+});
